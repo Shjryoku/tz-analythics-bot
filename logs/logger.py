9636322fe02ef70a82a365076a623b95a6caec83
shjryoku <shjryoku@gmail.com>
2025-12-19 00:54:23 +0300

    Добавил отдельную папку для логов

--- logs/logger.py
+++ logs/logger.py
@@ -1,12 +1,15 @@
 import logging
+import os
 
 from logging.handlers import RotatingFileHandler
+
+os.makedirs("log_files", exist_ok=True)
 
 logger = logging.getLogger("error_logger")
 logger.setLevel(logging.ERROR)
 
 file_handler = RotatingFileHandler(
-    "errors.log", maxBytes=1024, backupCount=1, encoding="utf-8"
+    "log_files/errors.log", maxBytes=1024, backupCount=1, encoding="utf-8"
 )
 file_handler.setLevel(logging.ERROR)
 
